0183e80af3558da95bfd4483656741bf2033971f
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2023-10-11 23:10:11 +0200

    chore(CI): mv api folder into docs only if is present

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://kelvin-olaiya.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/scala-project-template/',
+  baseUrl: '/Prolog-as-scalaDSL/',
   // baseUrl: '/',
 
   // GitHub pages deployment config.
