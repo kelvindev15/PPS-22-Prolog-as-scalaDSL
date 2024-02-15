4c41dff5a41ec936cba2b167a63e69b2832e18d6
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2024-02-15 16:43:23 +0100

    fix(solver-tuprolog): add unimplemented methods on the term to 2pkt-Term visitor

--- src/main/scala/io/github/kelvindev15/prolog/solver/tuprolog/visitors/From2PKtTermVisitor.scala
+++ src/main/scala/io/github/kelvindev15/prolog/solver/tuprolog/visitors/From2PKtTermVisitor.scala
@@ -4,22 +4,11 @@ import io.github.kelvindev15.prolog.core.Constant.Atom
 import io.github.kelvindev15.prolog.core.PrologList.Cons
 import io.github.kelvindev15.prolog.core.*
 import it.unibo.tuprolog.core
-import it.unibo.tuprolog.core.{
-  TermVisitor,
-  Atom as KAtom,
-  Cons as KCons,
-  Constant as KConstant,
-  EmptyList as KEmptyList,
-  Integer as KInteger,
-  Real as KReal,
-  Struct as KStruct,
-  Term as KTerm,
-  Var as KVariable
-}
+import it.unibo.tuprolog.core.{Block, Clause, Directive, Empty, EmptyBlock, Fact, Indicator, Recursive, Rule, TermVisitor, Truth, Atom as KAtom, Cons as KCons, Constant as KConstant, EmptyList as KEmptyList, Integer as KInteger, Real as KReal, Struct as KStruct, Term as KTerm, Var as KVariable}
 
 import scala.jdk.CollectionConverters.*
 
-/** Converts the [[Term]] hierarchy to a tuProlog [[KTerm]] hierarchy */
+/** Converts the [[KTerm]] hierarchy to a tuProlog [[Term]] hierarchy */
 private[tuprolog] object From2PKtTermVisitor extends TermVisitor[Term]:
 
   override def defaultValue(term: KTerm): Term = term match
@@ -29,6 +18,7 @@ private[tuprolog] object From2PKtTermVisitor extends TermVisitor[Term]:
     case variable: KVariable   => visitVar(variable)
     case constant: KConstant   => visitConstant(constant)
     case struct: KStruct       => visitStruct(struct)
+    case _ => throw NotImplementedError(s"The 2p-kt $term term cannot be currently converted to a term")
 
   override def visitAtom(term: KAtom): Atom = Atom(term.getValue)
 
@@ -51,3 +41,35 @@ private[tuprolog] object From2PKtTermVisitor extends TermVisitor[Term]:
   override def visitConstant(term: KConstant): Term = term match
     case integer: KInteger => Constant(integer.getValue.toInt)
     case double: KReal     => Constant(double.getValue.toDouble)
+
+  override def visitBlock(block: Block): Term = defaultValue(block)
+
+  override def visitClause(clause: Clause): Term = defaultValue(clause)
+
+  override def visitCollection(recursive: Recursive): Term = defaultValue(recursive)
+
+  override def visitDirective(directive: Directive): Term = defaultValue(directive)
+
+  override def visitEmpty(empty: Empty): Term = defaultValue(empty)
+
+  override def visitEmptyBlock(emptyBlock: EmptyBlock): Term = defaultValue(emptyBlock)
+
+  override def visitFact(fact: Fact): Term = defaultValue(fact)
+
+  override def visitIndicator(indicator: Indicator): Term = defaultValue(indicator)
+
+  override def visitInteger(integer: KInteger): Term = defaultValue(integer)
+
+  override def visitList(list: core.List): Term = defaultValue(list)
+
+  override def visitNumeric(numeric: core.Numeric): Term = defaultValue(numeric)
+
+  override def visitReal(real: KReal): Term = defaultValue(real)
+
+  override def visitRule(rule: Rule): Term = defaultValue(rule)
+
+  override def visitTerm(term: KTerm): Term = defaultValue(term)
+
+  override def visitTruth(truth: Truth): Term = defaultValue(truth)
+
+  override def visitTuple(tuple: core.Tuple): Term = defaultValue(tuple)
